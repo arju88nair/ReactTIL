33d7a1ca1f1787ba16cb626e5c611f8f3d23b243
Your Name <arju88nair@gmail.com>
2021-07-06 21:39:44 +0530

    ckeditor css style

--- src/views/NewNotesPage/Editor.jsx
+++ src/views/NewNotesPage/Editor.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
-import Editor from 'ckeditor5-custom-build/build/ckeditor';
-import {CKEditor} from '@ckeditor/ckeditor5-react'
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import {Container, CssBaseline} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
     toolbar: theme.mixins.toolbar,
@@ -12,35 +13,31 @@ const useStyles = makeStyles((theme) => ({
     },
 
 }));
-const editorConfiguration = {
-    toolbar: ['bold', 'italic']
-};
 
 
 export function NewEditor() {
     const classes = useStyles();
     return (
-
         <main className={classes.content}>
             <div className={classes.toolbar}/>
+
             <CKEditor
-                editor={Editor}
-                config={editorConfiguration}
+                editor={ ClassicEditor }
                 data="<p>Hello from CKEditor 5!</p>"
-                onReady={editor => {
+                onReady={ editor => {
                     // You can store the "editor" and use when it is needed.
-                    console.log('Editor is ready to use!', editor);
-                }}
-                onChange={(event, editor) => {
+                    console.log( 'Editor is ready to use!', editor );
+                } }
+                onChange={ ( event, editor ) => {
                     const data = editor.getData();
-                    console.log({event, editor, data});
-                }}
-                onBlur={(event, editor) => {
-                    console.log('Blur.', editor);
-                }}
-                onFocus={(event, editor) => {
-                    console.log('Focus.', editor);
-                }}
+                    console.log( { event, editor, data } );
+                } }
+                onBlur={ ( event, editor ) => {
+                    console.log( 'Blur.', editor );
+                } }
+                onFocus={ ( event, editor ) => {
+                    console.log( 'Focus.', editor );
+                } }
             />
         </main>
     );
